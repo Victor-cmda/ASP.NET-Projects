a94b6d1546e139346218ab8e8a16070575ddaa0d
Victor-cmda <playvitinho7@gmail.com>
2022-01-20 23:15:49 -0300

    Other Entities

--- MVC_SalesWeb/Migrations/20220121021347_OtherEntities.Designer.cs
+++ MVC_SalesWeb/Migrations/20220121021347_OtherEntities.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MVC_SalesWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVC_SalesWeb.Migrations
 {
     [DbContext(typeof(MVC_SalesWebContext))]
-    partial class MVC_SalesWebContextModelSnapshot : ModelSnapshot
+    [Migration("20220121021347_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MVC_SalesWeb/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using MVC_SalesWeb.Models.Enums;
+using System;
+
+namespace MVC_SalesWeb.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amont { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+            
+        }
+
+        public SalesRecord(int id, DateTime date, double amont, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amont = amont;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
