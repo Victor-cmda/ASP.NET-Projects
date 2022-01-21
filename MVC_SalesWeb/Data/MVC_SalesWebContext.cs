a45186ed568e36d2de22c4a2064605d9ffc72f63
Victor-cmda <playvitinho7@gmail.com>
2022-01-20 22:18:57 -0300

    Adicionando Banco de Dados MySQL

---
+++ MVC_SalesWeb/Data/MVC_SalesWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_SalesWeb.Models;
+
+namespace MVC_SalesWeb.Data
+{
+    public class MVC_SalesWebContext : DbContext
+    {
+        public MVC_SalesWebContext (DbContextOptions<MVC_SalesWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_SalesWeb.Models.Department> Department { get; set; }
+    }
+}
--- MVC_SalesWeb/Startup.cs
+++ MVC_SalesWeb/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_SalesWeb.Data;
 
 namespace MVC_SalesWeb
 {
@@ -31,8 +33,11 @@ namespace MVC_SalesWeb
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MVC_SalesWebContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("MVC_SalesWebContext"), builder =>
+                        builder.MigrationsAssembly("MVC_SalesWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
