ed7c0f84806dc88a8e1986df509ab17b2223108a
Victor-cmda <playvitinho7@gmail.com>
2022-02-03 22:54:53 -0300

    error page

--- MVC_SalesWeb/Startup.cs
+++ MVC_SalesWeb/Startup.cs
@@ -12,6 +12,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MVC_SalesWeb.Data;
 using MVC_SalesWeb.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace MVC_SalesWeb
 {
@@ -45,8 +47,18 @@ namespace MVC_SalesWeb
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS },
+            };
+            app.UseRequestLocalization(localizationOptions);
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
