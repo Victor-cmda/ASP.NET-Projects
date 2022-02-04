c8979b714c8c6aa411df5a09db62dad73a5f6570
Victor-cmda <playvitinho7@gmail.com>
2022-02-04 00:13:11 -0300

    validation

--- MVC_SalesWeb/Models/Seller.cs
+++ MVC_SalesWeb/Models/Seller.cs
@@ -8,17 +8,30 @@ namespace MVC_SalesWeb.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60, MinimumLength = 10, ErrorMessage ="{0} nome precisa ser maior que {2} caracteres")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Entre com um e-mail válido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Range(100.0,50000.0, ErrorMessage = "O Salário Base deve ser entre {1} e {2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
+
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
