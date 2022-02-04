ed18a47cdc139ebc99f2bcca68cabda88e68b27d
Victor-cmda <playvitinho7@gmail.com>
2022-02-04 01:21:07 -0300

    exception error delete seller

--- MVC_SalesWeb/Services/SellerService.cs
+++ MVC_SalesWeb/Services/SellerService.cs
@@ -21,7 +21,7 @@ namespace MVC_SalesWeb.Services
             return await _context.Seller.ToListAsync();
         }
         public async Task InsertAsync(Seller obj)
-        {         
+        {
             _context.Add(obj);
             await _context.SaveChangesAsync();
         }
@@ -31,9 +31,16 @@ namespace MVC_SalesWeb.Services
         }
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
         }
         public async Task UpdateAsync(Seller obj)
         {
@@ -44,8 +51,8 @@ namespace MVC_SalesWeb.Services
             }
             try
             {
-            _context.Update(obj);
-            await _context.SaveChangesAsync();
+                _context.Update(obj);
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException e)
             {
