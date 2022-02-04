64371873a7deb8a4ad5336b0b5c4ec048fc4fbec
Victor-cmda <playvitinho7@gmail.com>
2022-02-04 01:04:15 -0300

    async and await

--- MVC_SalesWeb/Services/DepartmentService.cs
+++ MVC_SalesWeb/Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using MVC_SalesWeb.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MVC_SalesWeb.Services
 {
@@ -13,9 +15,9 @@ namespace MVC_SalesWeb.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
